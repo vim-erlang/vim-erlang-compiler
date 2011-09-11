0d202350327510298c0a959824c549d489e4c7e1
Ricardo Catalinas Jiménez <jimenezrick@gmail.com>
2011-09-11 20:54:25 +0200

    Checks Erlang sources with a stronger compiler options
    If it is too heavy for you to compile each time the file is saved,
    disable the errors check with :ErlangDisableShowErrors

--- erlang_check.erl
+++ erlang_check.erl
@@ -1,5 +1,12 @@
 #!/usr/bin/env escript
 
 main([Filename]) ->
-    compile:file(Filename, [basic_validation, report, {i, "include"},
-            {i, "../include"}, {d, 'TEST'}, {d, 'DEBUG'}]).
+    compile:file(Filename, [strong_validation,
+                            warn_export_all,
+                            warn_export_vars,
+                            warn_shadow_vars,
+                            warn_obsolete_guard,
+                            warn_unused_import,
+                            report,
+                            {i, "include"}, {i, "../include"},
+                            {d, 'TEST'}, {d, 'DEBUG'}]).
