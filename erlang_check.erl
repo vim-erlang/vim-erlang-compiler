0e02cf719b9c0762680cff90f9d56d336b7167f9
Ricardo <ricardo@workstation.(none)>
2011-11-08 14:07:32 +0100

    Improves a little erlang_check.erl

--- erlang_check.erl
+++ erlang_check.erl
@@ -1,12 +1,15 @@
 #!/usr/bin/env escript
 
 main([Filename]) ->
-    compile:file(Filename, [strong_validation,
-                            warn_export_all,
-                            warn_export_vars,
-                            warn_shadow_vars,
-                            warn_obsolete_guard,
-                            warn_unused_import,
-                            report,
-                            {i, "include"}, {i, "../include"},
-                            {d, 'TEST'}, {d, 'DEBUG'}]).
+    Dir  = filename:dirname(Filename),
+    Opts = [strong_validation,
+            warn_export_all,
+            warn_export_vars,
+            warn_shadow_vars,
+            warn_obsolete_guard,
+            warn_unused_import,
+            report,
+            {i, Dir ++ "/include"},
+            {i, Dir ++ "/../include"},
+            {d, 'TEST'}, {d, 'DEBUG'}],
+    compile:file(Filename, Opts).
