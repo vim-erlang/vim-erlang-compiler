6f9f0d40d7331a24e9355af5652ef0ba4aa82cd6
Ricardo Catalinas Jiménez <jimenezrick@gmail.com>
2012-01-07 20:48:20 +0100

    Takes `erl_opts' from rebar.config
    Takes Rebar compile flags when doing a syntax check.

--- erlang_check.erl
+++ erlang_check.erl
@@ -1,15 +1,21 @@
 #!/usr/bin/env escript
 
 main([Filename]) ->
-    Dir  = filename:dirname(Filename),
-    Opts = [strong_validation,
-            warn_export_all,
-            warn_export_vars,
-            warn_shadow_vars,
-            warn_obsolete_guard,
-            warn_unused_import,
-            report,
-            {i, Dir ++ "/include"},
-            {i, Dir ++ "/../include"},
-            {d, 'TEST'}, {d, 'DEBUG'}],
-    compile:file(Filename, Opts).
+    Directory = filename:dirname(Filename),
+    Defaults = [strong_validation,
+        warn_export_all,
+        warn_export_vars,
+        warn_shadow_vars,
+        warn_obsolete_guard,
+        warn_unused_import,
+        report,
+        {i, Directory ++ "/include"},
+        {i, Directory ++ "/../include"},
+        {d, 'TEST'}, {d, 'DEBUG'}],
+    case file:consult("rebar.config") of
+        {ok, Terms} ->
+            RebarOpts = proplists:get_value(erl_opts, Terms, []),
+            compile:file(Filename, Defaults ++ RebarOpts);
+        _ ->
+            compile:file(Filename, Defaults)
+    end.
