07e6e67dd14f41cf986537b1281c782504cc39fa
Ricardo Catalinas Jiménez <jimenezrick@gmail.com>
2012-01-12 20:46:58 +0100

    Refactor Erlang code

--- erlang_check.erl
+++ erlang_check.erl
@@ -1,21 +1,21 @@
 #!/usr/bin/env escript
 
-main([Filename]) ->
-    Directory = filename:dirname(Filename),
-    Defaults = [strong_validation,
+main([File]) ->
+    Dir = filename:dirname(File),
+    Defs = [strong_validation,
         warn_export_all,
         warn_export_vars,
         warn_shadow_vars,
         warn_obsolete_guard,
         warn_unused_import,
         report,
-        {i, Directory ++ "/include"},
-        {i, Directory ++ "/../include"},
+        {i, Dir ++ "/include"},
+        {i, Dir ++ "/../include"},
         {d, 'TEST'}, {d, 'DEBUG'}],
     case file:consult("rebar.config") of
         {ok, Terms} ->
             RebarOpts = proplists:get_value(erl_opts, Terms, []),
-            compile:file(Filename, Defaults ++ RebarOpts);
+            compile:file(File, Defs ++ RebarOpts);
         _ ->
-            compile:file(Filename, Defaults)
+            compile:file(File, Defs)
     end.
