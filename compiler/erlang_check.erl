370a3b857061ade0587ded4c0907839dfb692cb6
Csaba Hoch <csaba.hoch@gmail.com>
2017-02-13 08:05:42 +0100

    (#35) Fix relative rebar3 path
    When the code found rebar3 with a relative path, the subsequent
    file:set_cwd call meant that we couldn't find that rebar3 executable any
    more.
    
    Now we convert the relative path to absolute so we find the rebar3
    executable even after set_cwd.
    
    Closes #35.

--- compiler/erlang_check.erl
+++ compiler/erlang_check.erl
@@ -563,7 +563,10 @@ process_rebar3_config(ConfigPath, Terms) ->
             % give an explicit error instead of proceeding anyway.
             log_error("rebar3 executable not found.~n"),
             error;
-        {ok, Rebar3} ->
+        {ok, Rebar3Rel} ->
+            log("rebar3 executable found: ~s~n", [Rebar3Rel]),
+            Rebar3 = filename:absname(Rebar3Rel),
+            log("Absolute path to rebar3 executable: ~s~n", [Rebar3]),
             % load the profile used by rebar3 to print the dependency path list
             Profile = rebar3_get_profile(Terms),
             % "rebar3 path" prints all paths that belong to the project; we add
@@ -573,9 +576,10 @@ process_rebar3_config(ConfigPath, Terms) ->
             % https://github.com/erlang/rebar3/issues/1143.
             {ok, Cwd} = file:get_cwd(),
             file:set_cwd(ConfigPath),
-            Paths = os:cmd(
-                      io_lib:format("QUIET=1 ~p as ~p path", [Rebar3, Profile])
-                     ),
+            Cmd = io_lib:format("QUIET=1 ~p as ~p path", [Rebar3, Profile]),
+            log("Call: ~s~n", [Cmd]),
+            Paths = os:cmd(Cmd),
+            log("Result: ~s~n", [Paths]),
             file:set_cwd(Cwd),
             CleanedPaths = [absname(ConfigPath, SubDir)
                             || SubDir <- string:tokens(Paths, " ")],
@@ -589,8 +593,11 @@ process_rebar3_config(ConfigPath, Terms) ->
               fun({ProfileName, Deps}) ->
                       Apps = string:join([atom_to_list(D) || D <- Deps], ","),
                       file:set_cwd(ConfigPath),
-                      ProfilePaths = os:cmd(io_lib:format("QUIET=1 ~p as ~p path --app=~s",
-                                                          [Rebar3, ProfileName, Apps])),
+                      Cmd = io_lib:format("QUIET=1 ~p as ~p path --app=~s",
+                                          [Rebar3, ProfileName, Apps]),
+                      log("Call: ~s~n", [Cmd]),
+                      ProfilePaths = os:cmd(Cmd),
+                      log("Result: ~s~n", [Paths]),
                       file:set_cwd(Cwd),
                       Cleaned = [absname(ConfigPath, SubDir)
                                  || SubDir <- string:tokens(ProfilePaths, " ")],
